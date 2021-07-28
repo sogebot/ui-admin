18e299d74229e6337d9d379eeb9c992a2453cf98
Michal Orlik <sogehige@gmail.com>
2021-07-28 13:59:46 +0200

    fix(widgets): remove empty space in widget tabs

--- components/widgets.vue
+++ components/widgets.vue
@@ -1,42 +1,39 @@
 <template>
   <div>
-    <v-tabs
-      v-model="tab"
-      height="36"
-      background-color="blue-grey darken-4"
-    >
-      <v-tab>{{ translate('widget-title-eventlist') }}</v-tab>
-      <v-tab v-if="$store.state.$systems.find(o => o.name === 'songs').enabled">
-        {{ translate('widget-title-ytplayer') }}
-      </v-tab>
-      <v-tab v-if="$store.state.$systems.find(o => o.name === 'queue').enabled">
-        {{ translate('widget-title-queue') }}
-      </v-tab>
-      <v-tab v-if="$store.state.$systems.find(o => o.name === 'raffles').enabled">
-        {{ translate('widget-title-raffles') }}
-      </v-tab>
-      <v-tab v-if="$store.state.$integrations.find(o => o.name === 'twitter').enabled">
-        {{ translate('widget-title-social') }}
-      </v-tab>
-      <v-tab>
-        {{ translate('widget-title-custom') }}
-      </v-tab>
-      <v-spacer />
-      <v-tooltip v-if="!isPopout" bottom>
-        <template #activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            href="#/popout/dashboard"
-            target="_blank"
-            v-on="on"
-          >
-            <v-icon>{{ mdiOpenInNew }}</v-icon>
-          </v-btn>
-        </template>
-        <span>Popout</span>
-      </v-tooltip>
-    </v-tabs>
+    <v-row no-gutters class="flex-nowrap">
+      <v-col cols="auto" v-if="!isPopout" order="last" class="flex-grow-0">
+        <div class="blue-grey darken-4">
+          <v-tooltip bottom>
+            <template #activator="{ on, attrs }">
+              <v-btn icon v-bind="attrs" href="#/popout/dashboard" target="_blank" v-on="on">
+                <v-icon>{{ mdiOpenInNew }}</v-icon>
+              </v-btn>
+            </template>
+            <span>Popout</span>
+          </v-tooltip>
+        </div>
+      </v-col>
+      <v-col cols="12" class="flex-shrink-1 flex-grow-0" style="min-width: 100px; max-width: 100%;">
+        <v-tabs v-model="tab" height="36" background-color="blue-grey darken-4" center-active>
+          <v-tab>{{ translate('widget-title-eventlist') }}</v-tab>
+          <v-tab v-if="$store.state.$systems.find(o => o.name === 'songs').enabled">
+            {{ translate('widget-title-ytplayer') }}
+          </v-tab>
+          <v-tab v-if="$store.state.$systems.find(o => o.name === 'queue').enabled">
+            {{ translate('widget-title-queue') }}
+          </v-tab>
+          <v-tab v-if="$store.state.$systems.find(o => o.name === 'raffles').enabled">
+            {{ translate('widget-title-raffles') }}
+          </v-tab>
+          <v-tab v-if="$store.state.$integrations.find(o => o.name === 'twitter').enabled">
+            {{ translate('widget-title-social') }}
+          </v-tab>
+          <v-tab>
+            {{ translate('widget-title-custom') }}
+          </v-tab>
+        </v-tabs>
+      </v-col>
+    </v-row>
     <v-tabs-items v-model="tab">
       <v-tab-item>
         <events />
