f1c4d73acb71ec0e7bbef6c676a1feebd0d3bba8
Michal Orlik <sogehige@gmail.com>
2021-08-16 20:53:20 +0200

    UPDATE

---
+++ components/registry/overlays/item.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-sheet
+    :color="selectedSync === item.id ? 'primary' : 'grey darken2'"
+    :height="item.height * ratio"
+    :width="item.width * ratio"
+    style="position: absolute; cursor: grab; user-select: none;"
+    :style="{
+      left: `${item.alignX * ratio}px`,
+      top: `${item.alignY * ratio}px`,
+    }"
+    @click="selectedSync = selectedSync === item.id ? null : item.id"
+  >
+    <v-slide-x-transition>
+      <v-sheet v-if="selectedSync === item.id" color="grey darken-4" style="position: absolute; right: -37px; height: 100%">
+        <v-btn icon block>
+          <v-icon>{{ mdiWrench }}</v-icon>
+        </v-btn>
+        <v-btn color="red" icon block>
+          <v-icon>{{ mdiDelete }}</v-icon>
+        </v-btn>
+      </v-sheet>
+    </v-slide-x-transition>
+    <div class="text-right pa-1 text-caption">
+      {{ item.width }}x{{ item.height }}<br>
+      <v-icon x-small>
+        {{ mdiFormatHorizontalAlignRight }}
+      </v-icon> {{ item.alignX }}
+      <v-icon x-small>
+        {{ mdiFormatVerticalAlignBottom }}
+      </v-icon> {{ item.alignY }}
+    </div>
+    <div class="center text-button text-truncate">
+      {{ item.type }}
+    </div>
+  </v-sheet>
+</template>
+
+<script lang="ts">
+import {
+  mdiCircle, mdiCircleOutline, mdiDelete, mdiEyeOutline, mdiFormatHorizontalAlignRight, mdiFormatVerticalAlignBottom, mdiWrench,
+} from '@mdi/js';
+import {
+  defineComponent, ref, watch,
+} from '@vue/composition-api';
+
+export default defineComponent({
+  props: {
+    item:     Object,
+    selected: [Object, String],
+    ratio:    Number,
+  },
+  setup (props, ctx) {
+    const selectedSync = ref(props.selected);
+    watch(() => props.selected, (val) => {
+      selectedSync.value = val;
+    });
+    watch(selectedSync, (val) => {
+      ctx.emit('update:selected', val);
+    });
+
+    return {
+      selectedSync,
+
+      // icons
+      mdiCircle,
+      mdiCircleOutline,
+      mdiDelete,
+      mdiEyeOutline,
+      mdiFormatHorizontalAlignRight,
+      mdiFormatVerticalAlignBottom,
+      mdiWrench,
+    };
+  },
+});
+</script>
--- pages/registry/overlays2/index.vue
+++ pages/registry/overlays2/index.vue
@@ -1,57 +1,92 @@
 <template>
   <v-container fluid :class="{ 'pa-4': !$vuetify.breakpoint.mobile }">
-        <v-card>
-            <v-btn fab absolute right bottom color="primary" dark small style="z-index: 999">
-              <v-icon>{{ mdiPlus }}</v-icon>
-            </v-btn>
-          <v-responsive :aspect-ratio="16/9">
-            <v-card-text>
-              <v-sheet color="primary" height="100" width="200" style="position: absolute; cursor: grab; user-select: none;">
-                 <v-sheet color="grey darken-4" style="position: absolute; right: -35px; height: 100%">
-                  <v-btn icon block><v-icon>{{ mdiWrench }}</v-icon></v-btn>
-                  <v-btn color="red" icon  block><v-icon>{{ mdiDelete }}</v-icon></v-btn>
-                 </v-sheet>
-                 <div class="text-right pa-1 text-caption">100x200<br />
-                  <v-icon x-small>{{ mdiFormatHorizontalAlignRight }}</v-icon> 100
-                  <v-icon x-small>{{ mdiFormatVerticalAlignBottom }}</v-icon> 100
-                 </div>
-                 <div class="center text-button text-truncate">Eventlist</div>
-              </v-sheet>
-              <v-sheet color="grey darken-3" height="150" width="75" style="position: absolute; top: 200px; left: 100px; cursor: grab; user-select: none;">
-                 <div class="center text-button text-truncate">Item 2</div>
-              </v-sheet>
-            </v-card-text>
-          </v-responsive>
-        </v-card>
+      <v-responsive ref="responsive" style="overflow: inherit" :aspect-ratio="16/9" :max-height="height" :max-width="(height / 9) * 16">
+    <v-card height="100%" width="100%">
+      <v-card-text>
+        <item v-for="item of items" :key="item.id" :item="item" :selected.sync="selected" :ratio="ratio" />
+      </v-card-text>
+    </v-card>
+      <v-btn
+        fab
+        absolute
+        right
+        bottom
+        color="primary"
+        dark
+        small
+        style="z-index: 999"
+      >
+        <v-icon>{{ mdiPlus }}</v-icon>
+      </v-btn>
+      </v-responsive>
   </v-container>
 </template>
 
 <script lang="ts">
+import { mdiPlus } from '@mdi/js';
 import {
-  mdiCircle, mdiCircleOutline, mdiDelete, mdiEyeOutline, mdiFormatHorizontalAlignRight, mdiFormatVerticalAlignBottom, mdiPlus, mdiWrench,
-} from '@mdi/js';
-import { defineComponent } from '@nuxtjs/composition-api';
+  defineAsyncComponent, defineComponent, nextTick, onMounted, ref,
+} from '@nuxtjs/composition-api';
 import { ButtonStates } from '@sogebot/ui-helpers/buttonStates';
 import translate from '@sogebot/ui-helpers/translate';
 
 export default defineComponent({
+  components: { item: defineAsyncComponent(() => import('~/components/registry/overlays/item.vue')) },
   setup () {
+    const selected = ref(null);
+    const responsive = ref(null as null | { '$el': HTMLElement });
+    const ratio = ref(0);
+    const height = ref(1920);
+
+    const resize = (responsiveRef: typeof responsive.value) => {
+      if (!responsiveRef) {
+        return;
+      }
+      height.value = window.innerHeight - 100;
+      nextTick(() => {
+        ratio.value = responsiveRef.$el.clientHeight / 1080;
+      });
+    };
+
+    onMounted(() => {
+      const resizeListener = () => {
+        resize(responsive.value);
+      };
+      window.addEventListener('resize', resizeListener);
+      nextTick(() => {
+        resize(responsive.value);
+      });
+    });
+
+    const items = ref([{
+      id:     '1',
+      type:   'eventlist',
+      alignX: 180,
+      alignY: 880,
+      width:  1600,
+      height: 100,
+    }, {
+      id:     '2',
+      type:   'eventlist',
+      alignX: 200,
+      alignY: 300,
+      width:  200,
+      height: 200,
+    }]);
     return {
       // refs
       translate,
+      items,
+      selected,
       ButtonStates,
+      responsive,
+      ratio,
+      height,
 
       // functions
 
       // icons
       mdiPlus,
-      mdiWrench,
-      mdiEyeOutline,
-      mdiDelete,
-      mdiFormatHorizontalAlignRight,
-      mdiFormatVerticalAlignBottom,
-      mdiCircle,
-      mdiCircleOutline,
     };
   },
 });
