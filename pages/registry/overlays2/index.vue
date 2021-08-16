d4dd97b9fcdccebcd1265fe3e9c3aa169af842e9
Michal Orlik <sogehige@gmail.com>
2021-08-16 11:18:57 +0200

    update

--- pages/registry/overlays2/index.vue
+++ pages/registry/overlays2/index.vue
@@ -1,292 +1,107 @@
 <template>
-  <v-container
-    fluid
-    :class="{ 'pa-4': !$vuetify.breakpoint.mobile }"
-  >
-    <!--v-data-table
-      v-model="selected"
-      :show-select="selectable"
-      :loading="state.loading !== ButtonStates.success || state.saving"
-      :headers="headers"
-      :items-per-page="-1"
-      hide-default-footer
-      hide-default-header
-      :items="items"
-      @current-items="saveCurrentItems"
-      @click:row="addToSelectedItem"
-    >
-      <template #top>
-        <v-sheet
-          flat
-          color="dark"
-          class="my-2 pb-2 mt-0"
-        >
-          <v-row class="px-2" no-gutters>
-            <v-col cols="auto" align-self="center" class="pr-2">
-              <v-btn icon :color="selectable ? 'primary' : 'secondary'" @click="selectable = !selectable">
-                <v-icon>
-                  {{ mdiCheckBoxMultipleOutline }}
+  <v-container fluid :class="{ 'pa-4': !$vuetify.breakpoint.mobile }">
+    <v-row dense>
+      <v-col cols="auto">
+        <v-card>
+          <v-card-text class="pa-0">
+              <v-subheader style="height: 30px">Overlays</v-subheader>
+            <v-list dense class="pa-0">
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Eventlist</v-list-item-title>
+                </v-list-item-content>
+                <v-list-item-action>
+                  <div>
+                <v-icon
+                  color="grey lighten-1"
+                >
+                  {{ mdiWrench }}
                 </v-icon>
-              </v-btn>
-            </v-col>
-            <v-col cols="auto" align-self="center" class="ml-auto mt-2">
-              <v-row no-gutters>
-                <v-col>
-                  <template v-if="selected.length > 0">
-                    <v-dialog
-                      v-model="deleteDialog"
-                      max-width="500px"
-                    >
-                      <template #activator="{ on, attrs }">
-                        <v-btn
-                          color="error"
-                          class="mr-1"
-                          v-bind="attrs"
-                          v-on="on"
-                        >
-                          Delete {{ selected.length }} Item(s)
-                        </v-btn>
-                      </template>
+                <v-icon
+                  color="grey lighten-1"
+                >
+                  {{ mdiEyeOutline }}
+                </v-icon>
+                <v-icon
+                  color="grey lighten-1"
+                >
+                  {{ mdiDelete }}
+                </v-icon>
+                  </div>
+              </v-list-item-action>
+              </v-list-item>
 
-                      <v-card>
-                        <v-card-title>
-                          <span class="headline">Delete {{ selected.length }} Item(s)?</span>
-                        </v-card-title>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Item 2</v-list-item-title>
+                </v-list-item-content>
+                <v-list-item-action>
+                  <div>
+                <v-icon
+                  color="grey lighten-1"
+                >
+                  {{ mdiWrench }}
+                </v-icon>
+                <v-icon
+                  color="grey lighten-1"
+                >
+                  {{ mdiEyeOutline }}
+                </v-icon>
+                <v-icon
+                  color="grey lighten-1"
+                >
+                  {{ mdiDelete }}
+                </v-icon>
+                  </div>
+              </v-list-item-action>
+              </v-list-item>
+            </v-list>
 
-                        <v-card-text>
-                          <v-data-table
-                            :headers="headers"
-                            :items-per-page="-1"
-                            hide-default-footer
-                            hide-default-header
-                            :items="selected"
-                          >
-                            <template #[`item.arrow`]="{ }">
-                              <v-icon>{{ mdiChevronRight }}</v-icon>
-                            </template>
-                            <template #[`item.overlay`]="{ item }">
-                              {{ item.value }}
-                            </template>
-                          </v-data-table>
-                        </v-card-text>
-
-                        <v-card-actions>
-                          <v-spacer />
-                          <v-btn
-                            text
-                            @click="deleteDialog = false"
-                          >
-                            Cancel
-                          </v-btn>
-                          <v-btn
-                            color="error"
-                            text
-                            @click="deleteSelected"
-                          >
-                            Delete
-                          </v-btn>
-                        </v-card-actions>
-                      </v-card>
-                    </v-dialog>
-                  </template>
-                </v-col>
-                <v-col align-self="end">
-                  <v-btn
-                    color="primary"
-                    @click="addItem()"
-                  >
-                    Add item
-                  </v-btn>
-                </v-col>
-              </v-row>
-            </v-col>
-          </v-row>
-        </v-sheet>
-      </template>
-
-      <template #[`item.id`]="{ item }">
-        <code>{{ item.id }}</code>
-      </template>
-
-      <template #[`item.overlay`]="{ item }">
-        {{ item.value }}
-      </template>
-
-      <template #[`item.arrow`]="{ }">
-        <v-icon>{{ mdiChevronRight }}</v-icon>
-      </template>
-
-      <template #[`item.actions`]="{ item }">
-        <div style="width: max-content;">
-          <v-hover v-slot="{ hover }">
-            <v-btn
-              icon
-              :color="hover ? 'primary' : 'secondary lighten-3'"
-              nuxt
-              :to="`/registry/overlays/${item.id}`"
-            >
-              <v-icon>
-                {{ mdiPencil }}
-              </v-icon>
+            <v-btn block color="pink" dark>
+              <v-icon>{{ mdiPlus }}</v-icon>
             </v-btn>
-          </v-hover>
-          <v-hover v-slot="{ hover }">
-            <v-btn
-              icon
-              :color="hover ? 'primary' : 'secondary lighten-3'"
-              :href="'/overlays/' + item.id"
-              @click.stop
-            >
-              <v-icon>{{ mdiLink }}</v-icon>
-            </v-btn>
-          </v-hover>
-          <v-hover v-slot="{ hover }">
-            <v-btn
-              icon
-              :color="hover ? 'primary' : 'secondary lighten-3'"
-              :disabled="copied===item.id"
-              @click.stop="copied=item.id"
-            >
-              <v-icon v-if="copied !== item.id">
-                {{ mdiClipboard }}
-              </v-icon>
-              <v-icon v-else>
-                {{ mdiClipboardCheck }}
-              </v-icon>
-            </v-btn>
-          </v-hover>
-        </div>
-      </template>
-    </v-data-table-->
+          </v-card-text>
+        </v-card>
+        <v-card class="mt-2">
+          <v-card-text class="pa-0">
+              <v-subheader style="height: 30px;">Settings</v-subheader>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-responsive :aspect-ratio="16/9">
+            <v-card-text>
+            </v-card-text>
+          </v-responsive>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
 import {
-  mdiCheckBoxMultipleOutline, mdiChevronRight, mdiClipboard, mdiClipboardCheck, mdiLink, mdiPencil,
+  mdiDelete, mdiEyeOutline, mdiPlus, mdiWrench,
 } from '@mdi/js';
-import {
-  defineComponent, onMounted, ref, useContext, useRouter, watch,
-} from '@nuxtjs/composition-api';
+import { defineComponent } from '@nuxtjs/composition-api';
 import { ButtonStates } from '@sogebot/ui-helpers/buttonStates';
 import translate from '@sogebot/ui-helpers/translate';
-import { cloneDeep } from 'lodash';
-import { v4 } from 'uuid';
-
-import type { OverlayMappers } from '.bot/src/database/entity/overlay';
-import { addToSelectedItem } from '~/functions/addToSelectedItem';
-import api from '~/functions/api';
-import { EventBus } from '~/functions/event-bus';
 
 export default defineComponent({
   setup () {
-    const { $axios } = useContext();
-    const router = useRouter();
-    const items = ref([] as OverlayMappers[]);
-    const cacheItems = ref([] as OverlayMappers[]);
-
-    const selected = ref([] as OverlayMappers[]);
-    const copied = ref('');
-    const currentItems = ref([] as OverlayMappers[]);
-    const saveCurrentItems = (value: OverlayMappers[]) => {
-      currentItems.value = value;
-    };
-    const deleteDialog = ref(false);
-    const selectable = ref(false);
-
-    watch(selectable, (val) => {
-      if (!val) {
-        selected.value = [];
-      }
-    });
-
-    watch(copied, (val) => {
-      if (val.length > 0) {
-        EventBus.$emit('snack', 'success', 'Link copied to clipboard.');
-        navigator.clipboard.writeText(`${document.location.protocol}//${document.location.host}/overlays/${val}`);
-        setTimeout(() => {
-          copied.value = '';
-        }, 1000);
-      }
-    });
-
-    const state = ref({ saving: false, loading: ButtonStates.progress } as {
-      loading: number;
-      saving: boolean;
-    });
-
-    const headers = [
-      {
-        value: 'id', text: '', sortable: false,
-      },
-      {
-        value: 'arrow', text: '', sortable: false,
-      },
-      {
-        value: 'overlay', text: '', sortable: false,
-      },
-      {
-        value: 'actions', text: '', sortable: false,
-      },
-    ];
-
-    onMounted(() => {
-      refresh();
-    });
-
-    const refresh = () => {
-      api.get<OverlayMappers[]>($axios, `/api/v1/overlay/`)
-        .then((response) => {
-          items.value = cloneDeep(response.data.data);
-          cacheItems.value = cloneDeep(response.data.data);
-        })
-        .then(() => (state.value.loading = ButtonStates.success));
-    };
-
-    const deleteSelected = () => {
-      deleteDialog.value = false;
-      selected.value.forEach((item) => {
-        api.delete($axios, `/api/v1/overlay/${item.id}`).catch(() => {
-          return true;
-        });
-      });
-      refresh();
-
-      EventBus.$emit('snack', 'success', 'Data removed.');
-      selected.value = [];
-    };
-
-    const addItem = () => {
-      router.push(`/registry/overlays/${v4()}`);
-    };
-
     return {
       // refs
-      items,
-      state,
-      headers,
-      selected,
-      deleteSelected,
-      selectable,
-      deleteDialog,
       translate,
       ButtonStates,
-      copied,
 
       // functions
-      addToSelectedItem: addToSelectedItem(selected, 'id', currentItems),
-      addItem,
 
       // icons
-      mdiCheckBoxMultipleOutline,
-      mdiChevronRight,
-      mdiLink,
-      mdiClipboard,
-      mdiClipboardCheck,
-      mdiPencil,
-
-      // external functions
-      saveCurrentItems,
+      mdiPlus,
+      mdiWrench,
+      mdiEyeOutline,
+      mdiDelete,
     };
   },
 });
