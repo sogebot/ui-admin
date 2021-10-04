1299b39d1716e3cddcb2ceb1c18d7e804c9fe7bd
Michal Orlik <sogehige@gmail.com>
2021-10-04 16:06:29 +0200

    quickactions wip

--- plugins/apollo-config.js
+++ plugins/apollo-config.js
@@ -1,6 +1,16 @@
+import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
+
+import introspectionQueryResultData from '~/static/fragmentTypes.json';
+
+const fragmentMatcher = new IntrospectionFragmentMatcher({ introspectionQueryResultData });
+
+const cache = new InMemoryCache({ fragmentMatcher });
+
 export default () => {
   return {
     httpEndpoint: location.origin + '/graphql',
+
+    cache,
 
     /*
      * For permanent authentication provide `getAuth` function.
