8a19e0713b16b64cda2cbf5c967599155badec18
Michal Orlik <sogehige@gmail.com>
2022-03-17 17:44:03 +0100

    fix(microwidgets): show widgets even if user is not partner

--- components/microWidgets/twitch-bits.vue
+++ components/microWidgets/twitch-bits.vue
@@ -9,6 +9,28 @@
     :current="current.currentBits"
     :average="average.currentBits"
   />
+  <v-col
+    v-else
+    cols="6"
+    lg="2"
+    md="4"
+    sm="4"
+    style="padding: 2px;"
+  >
+    <v-card
+      tile
+      min-height="48"
+      elevation="5"
+      :loading="!isLoaded"
+    >
+      <v-card-title :key="timestamp" class="px-1 py-0 text-truncate" style="font-size: 1rem;">
+        {{ song || capitalize($t('not-available')) }}
+      </v-card-title>
+      <v-card-subtitle class="pa-1 pt-2 text-caption text-truncate">
+        {{ capitalize($t('bits')) }}
+      </v-card-subtitle>
+    </v-card>
+  </v-col>
 </template>
 
 <script lang="ts">
@@ -16,6 +38,7 @@ import {
   defineAsyncComponent,
   defineComponent,
 } from '@nuxtjs/composition-api';
+import { capitalize } from 'lodash'
 
 export default defineComponent({
   components: { withTrending: defineAsyncComponent(() => import('./commons/withTrending.vue')) },
@@ -27,5 +50,8 @@ export default defineComponent({
     average:         Object,
     broadcasterType: String,
   },
+  setup() {
+    return { capitalize }
+  }
 });
 </script>
